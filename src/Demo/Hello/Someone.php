379f578a6296407b57ac796bb77315ea74b9fbe9
3A532081 <tanshui601@gmail.com>
2018-10-10 00:12:06 +0800

    Someone--1010

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3A532081
+ * Date: 2018/10/10
+ * Time: 上午 12:09
+ */
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
