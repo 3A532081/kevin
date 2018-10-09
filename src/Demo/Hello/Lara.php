beb2ce49d2396b5c6959a5028a3066e8ee6861a7
3A532081 <tanshui601@gmail.com>
2018-10-10 00:10:56 +0800

    Lara--1010

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3A532081
+ * Date: 2018/10/10
+ * Time: 上午 12:08
+ */
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
