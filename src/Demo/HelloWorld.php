072a1063aa32db198727ed045c9a25f3eacb6463
3A532081 <tanshui601@gmail.com>
2018-10-10 00:08:05 +0800

    HelloWorld--1010

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3A532081
+ * Date: 2018/10/9
+ * Time: 下午 11:59
+ */
+      namespace Demo;
+
+      class HelloWorld
+      {
+          function __construct()
+          {
+              echo 'Hello World ~'.PHP_EOL;
+          }
+      }
